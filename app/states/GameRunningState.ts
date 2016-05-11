79d9e1af131f6357d4e855bb35ffd91aaff640e0
Gelio <voreny.gelio@gmail.com>
2016-05-11 21:28:26 +0200

    added enemies that move left

--- app/states/GameRunningState.ts
+++ app/states/GameRunningState.ts
@@ -1,14 +1,15 @@
-import { CONFIG } from '../config';
-import { Player } from '../Player';
+import {CONFIG} from '../config';
+import {Player} from '../Player';
 
 const SPEED = 5;
 
 export class GameRunningState extends Phaser.State {
-    game: Phaser.Game;
-    background: Phaser.Sprite;
-    fighterSprite: Phaser.Sprite;
-    cursors: Phaser.CursorKeys;
-    player: Player;
+    game:Phaser.Game;
+    background:Phaser.Sprite;
+    fighterSprite:Phaser.Sprite;
+    cursors:Phaser.CursorKeys;
+    player:Player;
+    enemies:Phaser.Group;
 
     constructor() {
         super();
@@ -25,12 +26,20 @@ export class GameRunningState extends Phaser.State {
 
         this.background = this.game.add.sprite(0, 0, 'space');
 
-
+        // Player
         this.fighterSprite = this.game.add.sprite(0, 0, 'fighter');
-        this.fighterSprite.crop(new Phaser.Rectangle(0, this.fighterSprite.height/3, this.fighterSprite.width, this.fighterSprite.height/3), false);
+        this.fighterSprite.crop(new Phaser.Rectangle(0, this.fighterSprite.height / 3, this.fighterSprite.width, this.fighterSprite.height / 3), false);
         this.game.physics.enable(this.fighterSprite, Phaser.Physics.ARCADE);
-
         this.player = new Player(this.fighterSprite);
+
+        // Enemies
+        this.enemies = new Phaser.Group(this.game);
+
+        for (var i = 0; i < 10; i++) {
+            this.enemies.create(Math.random()*this.game.width, Math.random()*this.game.height, 'enemy');
+        }
+        this.game.physics.enable(this.enemies, Phaser.Physics.ARCADE);
+        this.enemies.forEach((enemy: Phaser.Sprite) => enemy.body.velocity.set(-10, 0), this);
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
     }
@@ -40,13 +49,13 @@ export class GameRunningState extends Phaser.State {
 
         var acceleration = new Phaser.Point(0, 0);
 
-        if(this.cursors.down.isDown)
+        if (this.cursors.down.isDown)
             acceleration.y++;
-        if(this.cursors.up.isDown)
+        if (this.cursors.up.isDown)
             acceleration.y--;
-        if(this.cursors.left.isDown)
+        if (this.cursors.left.isDown)
             acceleration.x--;
-        if(this.cursors.right.isDown)
+        if (this.cursors.right.isDown)
             acceleration.x++;
 
         acceleration = acceleration.multiply(CONFIG.player.acceleration, CONFIG.player.acceleration);
